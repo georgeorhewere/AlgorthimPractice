ea6f08310e6d41a805b65c0610fbb255b8ad0300
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-01-19 13:38:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorthimPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ QuickFind.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorthimPractice
+{
+    public class QuickFind
+    {
+        int[] itemArray;
+
+        public QuickFind(int N)
+        {
+            itemArray = new int[N];
+            
+            for (int x =0; x< N; x++)
+            {
+                itemArray[x] = x;
+            }
+
+        }
+
+
+
+
+    }
+}
