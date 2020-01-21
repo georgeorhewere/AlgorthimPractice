0bc46e7c527109b59e0e2bb33f2d48bd1c99ac2e
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-01-21 22:40:10 +0100

    implemented the weighted quick union using weighting and path compression

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace AlgorthimPractice
         {
 
             //callQuickFind();
-            callQuickUnion();
+            //callQuickUnion();
+            callWeightedQuickUnion();
 
         }
 
@@ -36,9 +37,33 @@ namespace AlgorthimPractice
             stub.union(3, 4);
             stub.union(5, 6);
             stub.union(7, 6);
-            Console.WriteLine(stub.isConnected(5, 7));
+            Console.WriteLine(stub.isConnected(5, 7));        
+        }
 
-        
+        static void callWeightedQuickUnion()
+        {
+            Console.WriteLine("Weighted Quick Union Test!");
+            WeightedQuickUnion stub = new WeightedQuickUnion(10);
+            Console.WriteLine(stub.isConnected(1, 2));
+            stub.union(1, 2);
+            Console.WriteLine(stub.isConnected(1, 2));
+            stub.union(3, 4);
+            stub.union(5, 6);
+            stub.union(7, 6);
+            Console.WriteLine(stub.isConnected(5, 7));
+            Console.WriteLine("Number of Connected components " + stub.numberOfConnectedComponents());
+            Console.WriteLine("size of 5 " + stub.getRootSize(5));
+            Console.WriteLine("size of 3 " + stub.getRootSize(3));
+            Console.WriteLine("size of 4 " + stub.getRootSize(4));
+            stub.union(3, 5);
+            stub.union(4, 2);
+            Console.WriteLine("size of 5 " + stub.getRootSize(5));
+            Console.WriteLine("size of 3 " + stub.getRootSize(3));
+            Console.WriteLine("size of 4 " + stub.getRootSize(4));
+            Console.WriteLine("Number of Connected components " + stub.numberOfConnectedComponents());
+            stub.printArray();
+
+
         }
 
     }
---
+++ WeightedQuickUnion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorthimPractice
+{
+    public class WeightedQuickUnion
+    {
+        //Weighted Union maintains an extra array that keeps track of the size of the treee
+        int[] idArray;
+        int[] treeSize;
+        public WeightedQuickUnion(int N)
+        {
+            idArray = new int[N];
+            treeSize = new int[N];
+
+            for (int x = 0; x < N; x++)
+            {
+                idArray[x] = x;
+                treeSize[x] = 1;
+            }
+        }
+
+        //find root 
+        public int getRoot(int item)
+        {
+            //too much work to be done in this array
+            while (item != idArray[item])
+            {
+                //path compression
+                //set root to previously set root
+                idArray[item] = idArray[idArray[item]];
+                item = idArray[item];
+            }
+            return item;
+        }
+
+        //isConnected
+        public bool isConnected(int p, int q)
+        {
+            return getRoot(p) == getRoot(q);
+        }
+        ////union
+        public void union(int p, int q)
+        {
+            int idP = getRoot(p);
+            int idQ = getRoot(q);
+            //set the root of p to the root of q
+
+            if (idP == idQ)
+                return;
+            //weight by size
+            if(treeSize[idP] < treeSize[idQ])
+            {
+                idArray[idP] = idQ;
+                treeSize[idQ] += treeSize[idP];
+            }
+            else
+            {
+                idArray[idQ] = idP;
+                treeSize[idP] += treeSize[idQ];
+            }            
+        }
+
+        public int getRootSize(int p)
+        {
+            return treeSize[p];
+
+        }
+
+        public int numberOfConnectedComponents()
+        {
+            return idArray.Where(c => hasConnectedItems(c)).Distinct().Count();
+        }
+
+        private bool hasConnectedItems(int item)
+        {
+            //gives invalid count
+            return idArray.Where(x => x == idArray[item]).Count() > 1;
+        }
+
+        public void printArray()
+        {
+            foreach (var c in idArray)
+                Console.Write(c + " , ");
+
+            Console.WriteLine();
+
+        }
+    }
+}
